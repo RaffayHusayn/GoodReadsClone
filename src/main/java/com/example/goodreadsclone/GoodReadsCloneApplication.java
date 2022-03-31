105841b0e60b1e4af0a42d4d71cb14a621d539d0
Raffay Syed <raffayhusayn@gmail.com>
2022-03-31 19:41:10 -0400

    Started GoodReadsClone project with spring boot adding required dependencies

---
+++ src/main/java/com/example/goodreadsclone/GoodReadsCloneApplication.java
@@ -0,0 +1,13 @@
+package com.example.goodreadsclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodReadsCloneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoodReadsCloneApplication.class, args);
+    }
+
+}
