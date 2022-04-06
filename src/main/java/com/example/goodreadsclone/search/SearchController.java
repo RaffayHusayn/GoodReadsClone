7c60ec03f1557da966c95ad051c63219a475328e
Raffay Syed <raffayhusayn@gmail.com>
2022-04-06 17:47:07 -0400

    Changes in Search Controller:
    -- Increased the memory limit of the webClient using WebClientBuilder's
    Exchange Strategies
    -- Changed the url format from using @PathVariable
    "website.com/search/<search-query>" to using @RequestParam
    "website.com/search?query=<search-query>
    -- Limiting the output result shown on the page to just top 10 using
    streams

--- src/main/java/com/example/goodreadsclone/search/SearchController.java
+++ src/main/java/com/example/goodreadsclone/search/SearchController.java
@@ -1,12 +1,15 @@
 package com.example.goodreadsclone.search;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 public class SearchController {
@@ -15,16 +18,25 @@ public class SearchController {
 
     public SearchController(WebClient.Builder webClientBuilder) {
         //making use of the Search API available in the openLibarary website
-        this.webClient = webClientBuilder.baseUrl("http://openlibrary.org/search/authors.json").build();
+        this.webClient = webClientBuilder
+                .exchangeStrategies(ExchangeStrategies.builder()
+                        .codecs(configurer -> configurer.defaultCodecs().maxInMemorySize(16 * 1024 * 1024))
+                        .build())
+                .baseUrl("http://openlibrary.org/search.json").build();
+
     }
 
 
-    @GetMapping("/search/{query}")
-    public String getSearchResults(@PathVariable String query, Model model){
-        Mono<SearchResult> searchResultMono= this.webClient.get()
+    @GetMapping(value = "/search")
+    public String getSearchResults(@RequestParam String query, Model model) {
+        Mono<SearchResult> searchResultMono = this.webClient.get()
                 .uri("?q={query}", query).retrieve().bodyToMono(SearchResult.class);
         SearchResult searchResult = searchResultMono.block();
-        model.addAttribute("searchResult", searchResult);
+        List<SearchResultBook> booksLimited = searchResult.getDocs()
+                .stream()
+                .limit(10)
+                .collect(Collectors.toList());
+        model.addAttribute("searchResult", booksLimited);
         return "search";
     }
 }
