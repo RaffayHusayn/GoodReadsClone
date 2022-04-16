c05d0834e6317ac41afd3e4e680b8d75ec81b0df
Raffay Syed <raffayhusayn@gmail.com>
2022-04-16 18:38:16 -0400

    Adds code to populate the books_by_user table in the remote cassandra instance along with already populating the books_by_userid_and_bookid

--- src/main/java/com/example/goodreadsclone/userbooks/UserBooksController.java
+++ src/main/java/com/example/goodreadsclone/userbooks/UserBooksController.java
@@ -1,7 +1,12 @@
 package com.example.goodreadsclone.userbooks;
 
+import com.example.goodreadsclone.user.BooksByUser;
+import com.example.goodreadsclone.user.BooksByUserRepository;
+import com.example.goodreadsclone.work.Work;
+import com.example.goodreadsclone.work.WorkRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.client.endpoint.AbstractWebClientReactiveOAuth2AccessTokenResponseClient;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.MultiValueMap;
@@ -10,17 +15,29 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.time.LocalDate;
+import java.util.Optional;
 
 @Controller
 public class UserBooksController {
 
     private UserBooksRepository userBooksRepository;
+    private BooksByUserRepository booksByUserRepository;
+    private WorkRepository workRepository;
 
     @Autowired
     public void setUserBooksRepository(UserBooksRepository userBooksRepository) {
         this.userBooksRepository = userBooksRepository;
     }
 
+    @Autowired
+    public void setBooksByUserRepository(BooksByUserRepository booksByUserRepository){
+        this.booksByUserRepository = booksByUserRepository;
+    }
+
+    @Autowired
+    public void setWorkRepository(WorkRepository workRepository){
+        this.workRepository = workRepository;
+    }
     @PostMapping("/addUserBook")
     public ModelAndView addBookForUser(@RequestBody MultiValueMap<String, String> formData, @AuthenticationPrincipal OAuth2User principal) {
         UserBooks userBooks = new UserBooks();
@@ -46,7 +63,33 @@ public class UserBooksController {
         }
 
 
+        //--------------------- Saving the data in the UserBooks Cassandra table-------------------------//
         userBooksRepository.save(userBooks);
+
+        String bookId = formData.getFirst("bookId");
+        Optional<Work> workOptional= workRepository.findById(bookId);
+        if(!workOptional.isPresent()){
+            return new ModelAndView("redirect:/");
+        }
+        Work work = workOptional.get();
+
+        // time based UUID is already being set at the time of construction in the constructor
+        BooksByUser booksByUser = new BooksByUser();
+        booksByUser.setBookId(bookId);
+        booksByUser.setId(principal.getAttribute("login"));
+        booksByUser.setBookName(work.getTitle());
+        booksByUser.setAuthorName(work.getAuthorNames());
+        booksByUser.setCoverIds(work.getCoverIds());
+        booksByUser.setReadingStatus(formData.getFirst("readingStatus"));
+        booksByUser.setRating(Integer.parseInt(formData.getFirst("rating")));
+
+
+        System.out.println(work.getCoverIds());
+        System.out.println(work.getTitle());
+
+        booksByUserRepository.save(booksByUser);
+
+
 
         return new ModelAndView("redirect:/book/"+formData.getFirst("bookId"));
     }
